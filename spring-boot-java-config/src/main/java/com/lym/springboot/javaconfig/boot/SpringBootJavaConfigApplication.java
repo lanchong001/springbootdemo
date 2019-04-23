9f0e735a7945a4c79cddc2e0ef9f035f10144809
liuyuanming <liuyuanming@lbx.com>
2019-04-23 16:16:07 +0800

    Spring Boot Java Config 的使用

---
+++ spring-boot-java-config/src/main/java/com/lym/springboot/javaconfig/boot/SpringBootJavaConfigApplication.java
@@ -0,0 +1,13 @@
+package com.lym.springboot.javaconfig.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavaConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJavaConfigApplication.class, args);
+	}
+
+}
