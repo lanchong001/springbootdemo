eb5dc887acab7d4db52d0a095354a2898b7813f6
liuyuanming <liuyuanming1111@163.com>
2019-05-11 00:02:10 +0800

    spring boot shiro 通过 redis 实现 session 缓存, 从而实现多节点共享

---
+++ spring-boot-shiro-redis/src/main/java/com/lym/springboot/shiro/redis/SpringBootShiroRedisApplication.java
@@ -0,0 +1,13 @@
+package com.lym.springboot.shiro.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShiroRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShiroRedisApplication.class, args);
+	}
+
+}
