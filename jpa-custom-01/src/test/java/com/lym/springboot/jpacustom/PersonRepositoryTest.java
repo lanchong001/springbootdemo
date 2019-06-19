96a640c7e24043a7d8ddf2f660902eec28d25e22
lanchong001 <git@797321>
2019-06-19 15:59:23 +0800

    jpa-custom-01 jpa 命名方法查询测试

--- jpa-custom-01/src/test/java/com/lym/springboot/jpacustom/PersonRepositoryTest.java
+++ jpa-custom-01/src/test/java/com/lym/springboot/jpacustom/PersonRepositoryTest.java
@@ -9,7 +9,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.CollectionUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: in liuyuanming
@@ -28,7 +30,7 @@ public class PersonRepositoryTest {
      * 查询自定义库中的方法, 实时执行 Sql 语句
      */
     @Test
-    public void testFindListByNativeSql(){
+    public void testFindListByNativeSql() {
         List<Person> listByNativeSql = personRepository.findListByNativeSql("select * from person where name='lili'", Person.class);
         Assert.assertTrue(!CollectionUtils.isEmpty(listByNativeSql));
     }
@@ -44,7 +46,7 @@ public class PersonRepositoryTest {
      * 查询自定义实体类
      */
     @Test
-    public void testQueryAllUsers(){
+    public void testQueryAllUsers() {
         List<User> listByNativeSql = personRepository.queryAllUsers();
         Assert.assertTrue(!CollectionUtils.isEmpty(listByNativeSql));
     }
