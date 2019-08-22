7d0a777fe44af5d951865820e82c7d78cde221a7
liuyuanming <liuyuanming1111@163.com>
2019-08-22 16:32:44 +0800

    redis 单点redis 访问配置; 哨兵模式,集群配置;

--- spring-boot-redistemplate/src/main/java/com/lym/springboot/redistemplate/config/RedisConfig.java
+++ spring-boot-redistemplate/src/main/java/com/lym/springboot/redistemplate/config/RedisConfig.java
@@ -1,17 +1,49 @@
 package com.lym.springboot.redistemplate.config;
 
 import org.apereo.cas.model.WhileList;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("#{'${spring.redis.sentinel.nodes}'.split(',')}")
+    private List<String> nodes;
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.redis")
+    public JedisPoolConfig getRedisConfig(){
+        JedisPoolConfig config = new JedisPoolConfig();
+        return config;
+    }
+    @Bean
+    public RedisSentinelConfiguration sentinelConfiguration(){
+        RedisSentinelConfiguration redisSentinelConfiguration = new RedisSentinelConfiguration();
+        //配置matser的名称
+        redisSentinelConfiguration.master("mymaster");
+        //配置redis的哨兵sentinel
+        Set<RedisNode> redisNodeSet = new HashSet<>();
+        nodes.forEach(x->{
+            redisNodeSet.add(new RedisNode(x.split(":")[0],Integer.parseInt(x.split(":")[1])));
+        });
+        redisSentinelConfiguration.setSentinels(redisNodeSet);
+        return redisSentinelConfiguration;
+    }
 
     @Bean
     public RedisConnectionFactory jedisConnectionFactory() {
@@ -22,12 +54,20 @@ public class RedisConfig {
         return jedisConFactory;
     }
 
+    @Bean
+    public RedisConnectionFactory jedisConnectionFactory(JedisPoolConfig jedisPoolConfig,RedisSentinelConfiguration redisSentinelConfiguration) {
+        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory(redisSentinelConfiguration,jedisPoolConfig);
+        jedisConFactory.setPassword("1234");
+        jedisConFactory.setDatabase(0);
+        return jedisConFactory;
+    }
+
     @Bean(name = "redisTemplate")
-    public RedisTemplate<String, WhileList> redisTemplate() {
+    public RedisTemplate<String, WhileList> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisSerializer<String> string = new StringRedisSerializer();
         JdkSerializationRedisSerializer jdk = new JdkSerializationRedisSerializer();
         final RedisTemplate<String, WhileList> template = new RedisTemplate<String, WhileList>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(string);
         template.setValueSerializer(jdk);
         return template;
--- spring-boot-redistemplate/src/test/java/com/lym/springboot/redistemplate/config/RedisConfigTest.java
+++ spring-boot-redistemplate/src/test/java/com/lym/springboot/redistemplate/config/RedisConfigTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StringUtils;
 
 import java.net.MalformedURLException;
 
@@ -18,22 +19,30 @@ public class RedisConfigTest {
     @Autowired
     private RedisTemplate<String, WhileList> redisTemplate;
 
+    /**
+     * 白名单 redis key 前缀
+     */
+    private static final String CAS_WHITE_LIST = "CAS_WHITELIST:";
+
+    /**
+     * 执行单元测试添加白名单
+     */
     @Test
     public void testSet() {
-
-        String key1 = "10.252.0.210:31381";
+        String ip = "10.254.2.245:8002";
+        //redis key : 前缀 + ip : port
+        String key = String.format("%s%s", CAS_WHITE_LIST, ip);
         WhileList whileList1 = new WhileList();
+        // auth : 授权码 1: 已授权; 0: 取消授权;
         whileList1.setIsAuth(1);
-        whileList1.setUrl("10.252.0.210:31381");
-        redisTemplate.boundValueOps(key1).set(whileList1);
-
-//        String key2 = "app2.cas.com:8082";
-//        WhileList whileList2 = new WhileList();
-//        whileList2.setIsAuth(0);
-//        whileList2.setUrl("app2.cas.com:8082");
-//        redisTemplate.boundValueOps(key2).set(whileList2);
+        // url : 白名单地址;
+        whileList1.setUrl(ip);
+        redisTemplate.boundValueOps(key).set(whileList1);
     }
 
+    /**
+     * redisTemplate get 获取
+     */
     @Test
     public void testGet() {
 
@@ -44,9 +53,11 @@ public class RedisConfigTest {
         }
     }
 
+    /**
+     * 获取url 主机名与端口号
+     */
     @Test
     public void testUrl() {
-
         try {
             java.net.URL url = new java.net.URL("http://blog.csdn.net:9090/zhujianlin1990");
             String host = url.getHost();// 获取主机名
@@ -56,6 +67,32 @@ public class RedisConfigTest {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+    }
 
+    /**
+     * 字符串截图测试
+     */
+    @Test
+    public void testSplit() {
+//        String url = "http://castest.lbxdrugs.com:18080/cas/ConfirmLogin/TST-23-qESkYEjQ-bCPIvgxz-g9vo1Qw5D2gmIq/";
+//        String url = "http://castest.lbxdrugs.com:18080/cas/ConfirmLogin/TST-23-qESkYEjQ-bCPIvgxz-g9vo1Qw5D2gmIq";
+//        String url = "http://castest.lbxdrugs.com:18080/cas/ConfirmLogin/234234234";
+//        String url = "http://castest.lbxdrugs.com:18080/cas/ConfirmLogin";
+//        String url = "";
+        String url = null;
+        System.out.printf(getClientId(url));
+    }
+
+    public String getClientId(String url) {
+        String result = "";
+        if (!StringUtils.isEmpty(url)) {
+            String[] urls = url.split("/ConfirmLogin/");
+            if (urls.length >= 2) {
+                if (!StringUtils.isEmpty(urls[1])) {
+                    result = urls[1].replace("/", "");
+                }
+            }
+        }
+        return result;
     }
 }
